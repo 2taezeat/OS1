6f6f320d41d25eaa120dda52eb3b2f9d7df13ca7
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-10-14 11:19:39 +0900

    201014_os_hw1(2)

--- OS1/1007-2016320131-이태경-1.c
+++ OS1/1007-2016320131-이태경-1.c
@@ -3,25 +3,26 @@
 
 int main(void)
 {
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
+	STARTUPINFO si; //new process의 특성
+	PROCESS_INFORMATION pi; // new process/thread에 대한 핸들/식별자
 
-	/**/
+	/*allocate memory*/
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
-	/**/
+	/*create child process*/
 	if (!CreateProcess(NULL, "C:\\WINDOWS\\system32\\mspaint.exe", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-	{
+	{ // use command line, command, don't inherit process handle, don't inherit thread handle, disable handle inheritance, no creation flags,
+		// use parent's environment block, use parent's existing directory
 		fprintf(stderr, "Create Process Failed");
 		return -1;
 	}
-	/**/
+	/*parent will wait for the child to complte */
 	WaitForSingleObject(pi.hProcess, INFINITE);
 	printf("Child Complete");
 
-	/**/
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
+	/*close handles*/
+	CloseHandle(pi.hProcess); // new process에 대한 핸들 닫기
+	CloseHandle(pi.hThread); // new threead에 대한 핸들 닫기
 
 }
